f6bfc9974b5f5ae820f5e5288981960c989f1d7d
Shadrack Afuvya Okoth <shdrckokoth@gmail.com>
2025-03-20 04:29:50 +0300

    initialization

---
+++ Mysql/answers.sql
@@ -0,0 +1,6 @@
+--line code to create a database salesDB
+CREATE DATABASE salesDB;
+
+
+--line code to drop a database Demo
+DROP DATABASE demo;
--- S.py
+++ S.py
@@ -1,1 +1,12 @@
-print"Hi")
+#plp training 
+#Variable initialization 
+a="1"
+b=2.0
+c=3.56
+x=int(c)
+print(x)
+for x in "tomato":
+    print(x)
+txt="I am chilling with Clare"
+if "Clare" in txt:
+    print("Yes 'Clare' is present ")
